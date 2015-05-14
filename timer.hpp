4186c625a35f11c2f578ad92358e06730620e9d8
Elie Zedeck <elie.zedeck@gmail.com>
2015-05-14 18:52:08 +0300

    Clean-up timer.hpp
    - Removed unnecessary includes
    - Added necessary inclusion of loop.hpp
    - Fix indentation (to my liking)
    - Fix to use plain 64-bit unsigned int for the timeouts

--- timer.hpp
+++ timer.hpp
@@ -1,56 +1,46 @@
 #pragma once
 
 #include "handle.hpp"
+#include "loop.hpp"
 #include "error.hpp"
-#include <chrono>
-#include <iostream>
 
 namespace uvpp
 {
-    class Timer : public handle<uv_timer_t>
+
+class Timer: public handle<uv_timer_t>
+{
+public:
+    Timer()
+            : handle()
     {
-    public:
-        Timer():
-            handle<uv_timer_t>()
-        {
-            uv_timer_init(uv_default_loop(), get());
-        }
+        uv_timer_init(uv_default_loop(), get());
+    }
 
-        Timer(loop& l):
-            handle<uv_timer_t>()
-        {
-            uv_timer_init(l.get(), get());
-        }
+    Timer(loop &l)
+            : handle()
+    {
+        uv_timer_init(l.get(), get());
+    }
 
-        error start(std::function<void()> callback, const std::chrono::duration<uint64_t, std::milli> &timeout, const std::chrono::duration<uint64_t, std::milli> &repeat) {
-			callbacks::store(get()->data, internal::uv_cid_timer, callback);
-        	return error(uv_timer_start(get(),
-        		[](uv_timer_t* handle){
-        			callbacks::invoke<decltype(callback)>(handle->data, internal::uv_cid_timer);
-        		},
-        		timeout.count(),
-        		repeat.count()
-        		));
-        }
+    bool start(std::function<void()> callback, const uint64_t timeout,
+            const uint64_t repeat)
+    {
+        callbacks::store(get()->data, internal::uv_cid_timer, callback);
+        return uv_timer_start(get(),
+                [](uv_timer_t* handle) {
+                    callbacks::invoke<decltype(callback)>(handle->data, internal::uv_cid_timer);
+                }, timeout, repeat);
+    }
 
-        error start(std::function<void()> callback, const std::chrono::duration<uint64_t, std::milli> &timeout) {
-        	std::cout << "timeout: " << timeout.count() << std::endl;
-			callbacks::store(get()->data, internal::uv_cid_timer, callback);
-        	return error(uv_timer_start(get(),
-        		[](uv_timer_t* handle){
-        			callbacks::invoke<decltype(callback)>(handle->data, internal::uv_cid_timer);
-        		},
-        		timeout.count(),
-        		0
-        		));
-        }        
+    bool stop()
+    {
+        return uv_timer_stop(get());
+    }
 
-        error stop() {
-        	return error(uv_timer_stop(get()));
-        }
+    bool again()
+    {
+        return uv_timer_again(get());
+    }
+};
 
-        error again() {
-        	return error(uv_timer_again(get()));
-        }
-    };
-}
+} // namespace uvpp
